41afb498f6c3532f7d79e3a8424e4f5e8c250ae7
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 19:29:56 +0200

    refactor: create DisplayWizards function component

--- client/src/pages/WizardList.jsx
+++ client/src/pages/WizardList.jsx
@@ -7,35 +7,55 @@ function WizardList() {
   const [wizards, setWizards] = useState([]);
 
   useEffect(() => {
-    async function loadWizards() {
-      try {
-        const res = await fetch(`${API_URL}/api/v1/wizards`);
-        if (res.ok) {
-          const data = await res.json();
-          setWizards(data.data); // This updates the UI automatically!
-        }
-      } catch (err) {
-        console.error("Error fetching wizards", err);
-      }
-    }
+      
+      fetch(`${API_URL}/api/v1/wizards`)
+      .then(response => {
+        if(!response.ok) throw new Error('Failed to fetch wizards')
+          return response.json();
+      })
+      .then(data => setWizards(data.data))
+      .catch(err => console.error(err))
+    
+      // async function getWizards() {
+      //   try {
+      //     const res = await fetch(`${API_URL}/api/v1/wizards`);
+      //     if (res.ok) {
+      //       const data = await res.json();
+      //       setWizards(data.data);
+      //     }
+      //   } catch (err) {
+      //     console.error("Error fetching wizards", err);
+      //   }
+      // }
 
-    loadWizards();
+      // getWizards();
   }, []);
+
+  function DisplayWizards() {
+    if(wizards.length === 0) {
+      return (
+      <div>
+      <p>API service is either down or no wizards found in the database...</p>
+      <a href={API_URL} target="_blank">Click here to wake up the API service then try again</a>
+      </div>
+      )
+    } else {
+      return (
+      <ul>
+      {wizards.map((wizard) => (
+        <li key={wizard.id}>
+          <Link to={`/wizards/${wizard.id}`}>{wizard.name}</Link>
+        </li>
+      ))}
+    </ul>
+      )
+    }
+  }
 
   return (
     <div>
       <h2>All Wizards</h2>
-      {wizards.length === 0 ? (
-        <p>Loading or no wizards found...</p>
-      ) : (
-        <ul>
-          {wizards.map((wizard) => (
-            <li key={wizard.id}>
-              <Link to={`/wizards/${wizard.id}`}>{wizard.name}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <DisplayWizards />
     </div>
   );
 }
