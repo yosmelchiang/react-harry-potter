3142e494189c4255d49a0f2e6c8725edcef4311d
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-24 19:35:26 +0200

    refactor: use a fetch with then instead of async an await, its simpler

--- client/src/pages/WizardDetail.jsx
+++ client/src/pages/WizardDetail.jsx
@@ -8,18 +8,30 @@ function WizardDetail() {
   const [wizard, setWizard] = useState(null);
 
   useEffect(() => {
-    async function loadWizard() {
-      try {
-        const res = await fetch(`${API_URL}/api/v1/wizards`);
-        const allWizards = await res.json();
-        const found = allWizards.data.find((w) => w.id === parseInt(id));
-        setWizard(found);
-      } catch (err) {
-        console.error("Error fetching wizard", err);
-      }
-    }
 
-    loadWizard();
+    fetch(`${API_URL}/api/v1/wizards`)
+    .then(response => {
+      if(!response.ok) throw new Error('Error fetching wizard')
+      return response.json();
+    })
+    .then(data => {
+      const wizard = data.data.find(w => w.id === +id)
+      setWizard(wizard)
+    })
+    .catch(err => console.error(err));
+
+    // async function loadWizard() {
+    //   try {
+    //     const res = await fetch(`${API_URL}/api/v1/wizards`);
+    //     const allWizards = await res.json();
+    //     const found = allWizards.data.find((w) => w.id === parseInt(id));
+    //     setWizard(found);
+    //   } catch (err) {
+    //     console.error("Error fetching wizard", err);
+    //   }
+    // }
+
+    // loadWizard();
   }, [id]);
 
   if (!wizard) return <p>Wizard not found...</p>;
